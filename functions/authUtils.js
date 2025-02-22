7bdd3c49ff9f5301b4c921a2d50e7595c4f86cb7
Tanishq0Sharma <sharmat2@tcnj.edu>
2025-02-22 18:18:24 -0500

    Update authUtils.js

--- functions/authUtils.js
+++ functions/authUtils.js
@@ -1,1 +1,21 @@
+const { AuthenticationClient } = require('auth0');
 
+const auth0 = new AuthenticationClient({
+    domain: process.env.AUTH0_DOMAIN,
+    clientId: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET
+});
+
+async function verifyToken(event) {
+    const token = event.headers.authorization?.replace('Bearer ', '');
+    if (!token) throw new Error('Unauthorized');
+
+    try {
+        const result = await auth0.getProfile(token);
+        return result;
+    } catch (error) {
+        throw new Error('Invalid token');
+    }
+}
+
+module.exports = { verifyToken };
